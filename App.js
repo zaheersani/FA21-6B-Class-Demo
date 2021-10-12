bf8585089a7608a65245069d1a91aa704874901b
zaheersani <sani.zaheer@gmail.com>
2021-10-12 17:50:53 +0500

    Components and Props

--- App.js
+++ App.js
@@ -2,11 +2,42 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Name = (props) => {
+  if (props.info) {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={{ fontSize: 16 }}>First Name: {props.info.fname}</Text>
+        <Text style={{ fontSize: 16 }}>Last Name: {props.info.lname}</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={{backgroundColor: 'yellow'}}>
+        <Text style={{color:'red', fontSize: 15}}>Name is not available</Text>
+      </View>
+    )
+  }
+  
+}
+
+class Age extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Age: {this.props.ageInNumbers}</Text>
+      </View>
+    )
+  }
+}
+
+function App() {
+  const infoObj = { fname: 'zaheer', lname: 'sani' }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Name />
+      <Name />
+      <Name info={infoObj} />
+      <Age ageInNumbers={23} />
     </View>
   );
 }
@@ -14,8 +45,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
