e1f7429516746503febe9ffb51deb70e82c2da32
zaheersani <sani.zaheer@gmail.com>
2021-11-09 15:32:48 +0500

    FlatList Example

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import {ScrollView, TextInput, TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native';
+import {ScrollView, FlatList, TextInput, TouchableOpacity, StyleSheet, Text, View, Button, ToastAndroid } from 'react-native';
 
 function App() {
-  var arr = [34, 23, 343, 232, 33, 22, 33, 77, 88, 88]
+  var arr = [34, 23, 343, 232, 33, 22, 33, 77, 88, 88, 22, 33, 77, 88, 88]
   return (
     <View style={styles.container}>
-      <ScrollView
-        style={{paddingTop: 20}}
-      >
-        <Text style={{fontSize: 54}}>toggle auto opening developer tools on startup (enabled)</Text>
-        <Text style={{ fontSize: 54 }}>toggle auto opening developer tools on startup (enabled)</Text>
-        <Text style={{ fontSize: 54 }}>toggle auto opening developer tools on startup (enabled)</Text>
-        {arr.map((i, index) =>
+      <FlatList
+        style={{paddingTop: 20, width: '100%', padding: 5}}
+        data={arr}
+        renderItem={({ item, index }) =>
           <TouchableOpacity
-            style={{width: '100%', height: 50, backgroundColor: 'red', padding: 5, margin: 5}}
+            style={{ backgroundColor: 'pink', margin: 10, width:'40%'}}
           >
-            <Text key={index} style={{ fontSize: 44 }}>Item: {i}, index: {index}</Text>
+            <Text key={index} style={{ fontSize: 34 }}>item: {item}</Text>
           </TouchableOpacity>
-        )}
-      </ScrollView>
+        }
+        ListHeaderComponent={<Text>Header</Text>}
+        ListFooterComponent={<Text>Footer</Text>}
+        numColumns={2}
+        refreshing={false}
+        onRefresh={()=>ToastAndroid.show('fetching data..', ToastAndroid.SHORT)}
+      />
     </View>
   );
 }
