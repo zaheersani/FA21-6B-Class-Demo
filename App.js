98a1e9b7fbfdf0ae2c3d356d11d9a6fdd0127af5
zaheersani <sani.zaheer@gmail.com>
2021-11-09 15:07:44 +0500

    ScrollView Example

--- App.js
+++ App.js
@@ -1,88 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import {TextInput, TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native';
-
-const MyButton = ({ text, bgcolor = 'blue', onTap}) => {
-  return (
-    <TouchableOpacity
-      style={{
-        backgroundColor: bgcolor,
-        width: 50,
-        height: 50,
-        borderRadius: 100,
-        justifyContent: 'center',
-        elevation: 10,
-        borderColor: 'grey',
-        borderWidth: 1
-      }}
-      activeOpacity={0.5}
-      onPress={ () => onTap(text)}
-    >
-      <Text
-        style={{ fontSize: 24, color: 'black', alignSelf: 'center' }}
-      >{text}</Text>
-    </TouchableOpacity>
-  )
-}
-
-const NumPad = () => {
-  const [getNum, setNum] = useState(0)
-  const [btnFeedback, setBtnFeedback] = useState('Not Presse')
-  const [text, setText] = useState('')
-
-  const onBtnPress = (n) => {
-    setNum(getNum + n)
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text
-        style={{ fontSize: 46, alignSelf: 'flex-end', paddingRight: 10 }}
-        selectable={true}
-        numberOfLines={1}
-      >{getNum}</Text>
-      <Text>{btnFeedback}</Text>
-      <Text>{text}</Text>
-      <TextInput
-        style={{
-          borderWidth: 1,
-          width: 150,
-          height: 50,
-          fontSize: 24,
-          borderColor: 'blue',
-          borderRadius: 10,
-          paddingLeft: 10
-        }}
-        onChangeText={(v) => setText(v)}
-        keyboardType={'number-pad'}
-      />
-      <View style={{
-        flexDirection: 'row',
-        padding: 10,
-        width: '70%',
-        // backgroundColor: 'red',
-        justifyContent: 'space-between'
-      }}>
-        {/* <Button title="1" onPress={() => setNum(getNum + "1")} /> */}
-        <MyButton text="1" bgcolor="red" onTap={onBtnPress} />
-        <MyButton text="2" onTap={onBtnPress} />
-        <MyButton text="3" onTap={onBtnPress} />
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Button title="4" />
-        <Button title="5" />
-        <Button title="6" />
-      </View>
-    </View>
-  )
-}
+import {ScrollView, TextInput, TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native';
 
 function App() {
-  const [isStart, setStart] = useState(false)
+  var arr = [34, 23, 343, 232, 33, 22, 33, 77, 88, 88]
   return (
     <View style={styles.container}>
-      {!isStart && <Button title="Start" onPress={() => setStart(true)} />}
-      {isStart && <NumPad />}
+      <ScrollView
+        style={{paddingTop: 20}}
+      >
+        <Text style={{fontSize: 54}}>toggle auto opening developer tools on startup (enabled)</Text>
+        <Text style={{ fontSize: 54 }}>toggle auto opening developer tools on startup (enabled)</Text>
+        <Text style={{ fontSize: 54 }}>toggle auto opening developer tools on startup (enabled)</Text>
+        {arr.map((i, index) =>
+          <TouchableOpacity
+            style={{width: '100%', height: 50, backgroundColor: 'red', padding: 5, margin: 5}}
+          >
+            <Text key={index} style={{ fontSize: 44 }}>Item: {i}, index: {index}</Text>
+          </TouchableOpacity>
+        )}
+      </ScrollView>
     </View>
   );
 }
